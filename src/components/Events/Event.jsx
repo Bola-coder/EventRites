728211397b3d20dac0189df073ac646b80daf0a7
Tesla <bolarinwaahmed22@gmail.com>
2022-04-30 10:29:26 +0100

    Added baic tailfind configuration and files

--- src/components/Events/Event.jsx
+++ src/components/Events/Event.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import EventHeader from "./EventHeader";
 
 const Events = () => {
   document.title = "EventRite | Events";
   return (
     <>
       <h1>Event Page</h1>
+      <EventHeader />
     </>
   );
 };
